a406e000a30e5159ee2ac0ed79166904f296b8d1
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-11-01 13:33:44 +0500

    Controller Added

--- BonusProject/Controllers/SearchController.cs
+++ BonusProject/Controllers/SearchController.cs
@@ -8,10 +8,17 @@ namespace BonusProject.Controllers
     public class SearchController : ControllerBase
     {
         [HttpGet]
-        public IActionResult GetTableNames(string shart)
+        public IActionResult FromOneDatabase(string shart)
         {
-            var res = Serarch.GetTableNames(shart);
+            var res = Serarch.FromOneDatabase(shart);
 
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public IActionResult FromAllDatabase(string shart)
+        {
+            var res = Serarch.FromAllDatabase(shart);
             return Ok(res);
         }
     }
